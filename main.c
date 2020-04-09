0ef0267dfa1494ee80ed94e18dc6cadd9319d5df
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-04-09 16:53:22 -0700

    updated main

--- main.c
+++ main.c
@@ -117,7 +117,7 @@ int main(int argc, char* argv[])
 	// WRTIE code here to create P1 as a USER process
 
 	while (1) {
-		printf("input command : [ls|cd|pwd|mkdir|creat|quit] ");
+		printf("input command : [ls|cd|pwd|mkdir|creat|rmdir|link|unlink|symlink|quit] ");
 		fgets(line, 128, stdin);
 		line[strlen(line) - 1] = 0;
 
