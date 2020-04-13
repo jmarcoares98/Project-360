478c1fa583acfff75e48f8a5082ec189ddd13c28
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-04-13 12:41:43 -0700

    Fixed mkdir, now works

--- mkdir_creat.c
+++ mkdir_creat.c
@@ -101,7 +101,7 @@ int mymkdir(MINODE* pip, char* name)
 	char* cp;
 	DIR* dp;
 	char buf[BLKSIZE];
-	int ino, bno, parent, mk;
+	int ino, bno, parent;
 
 	// 1. pip points at the parent minode[] of "/a/b", name is a string "c"
 	parent = pip->dev;
@@ -114,7 +114,7 @@ int mymkdir(MINODE* pip, char* name)
 
 	// 3. mip = iget(dev, ino);  load the inode into a minode[] (in order to
 	// wirte contents to the INODE in memory.
-	mip = iget(dev, ino);
+	mip = iget(parent, ino);
 
 	// 4. Write contents to mip->INODE to make it a DIR INODE.
     mip->INODE.i_mode = 0x41ED;		// DIR type and permissions
@@ -124,11 +124,10 @@ int mymkdir(MINODE* pip, char* name)
 	mip->INODE.i_links_count = 2;	        // Links count=2 because of . and ..
 	mip->INODE.i_atime = mip->INODE.i_ctime = mip->INODE.i_mtime = time(0L);  // set to current time
 	mip->INODE.i_blocks = 2;                	// LINUX: Blocks count in 512-byte chunks
-
-	for (int i = 0; i < 15; i++) //  i_block[1] to i_block[14] = 0;
-		mip->INODE.i_block[i] = 0;
-
 	mip->INODE.i_block[0] = bno;             // new DIR has one data block
+
+	for (int i = 1; i < 15; i++) //  i_block[1] to i_block[14] = 0;
+		mip->INODE.i_block[i] = 0;
 
 	mip->dirty = 1;               // mark minode dirty
 	mip->ino = ino;
@@ -137,119 +136,96 @@ int mymkdir(MINODE* pip, char* name)
 	iput(mip);                    // write INODE to disk
 
 	// 6. Write . and .. entries to a buf[ ] of BLKSIZE
-	memset(buf, 0, 1024);
+	memset(buf, 0, BLKSIZE);
 
 	dp = (DIR*)buf;
+	cp = buf;
+
 	dp->inode = ino;
 	strncpy(dp->name, ".", 1); // for . entries
 	dp->name_len = 1;
 	dp->rec_len = 12;
 
-	cp = buf + 12;
+	cp += dp->rec_len;
 	dp = (DIR*)cp;
 
 	dp->inode = pip->ino;
 	dp->name_len = 2;
+	dp->file_type = (u8)EXT2_FT_DIR; //EXT2 dit type
 	strncpy(dp->name, "..", 2); // for .. entries
 	dp->rec_len = BLKSIZE - 12;
 
 	put_block(parent, bno, buf);
 
-	ino = mip->ino;
 	// 7. Finally, enter name ENTRY into parent's directory
-	enter_name(pip, ino, name);
+	enter_name(pip, mip->ino, name);
 }
 
 int enter_name(MINODE* pip, int myino, char* myname)
 {
+	INODE* ip = &(pip->INODE);
+	char buf[BLKSIZE];
 	char* cp;
 	DIR* dp;
-	int idea_len, need_len, remain, i, bnum;
+	int idea_len = 0, need_len = 0, remain = 0, bnum = 0, i = 0;
 	int name_len = strlen(myname);
 
-	// update
-	memset(buf, 0, BLKSIZE);
+	for (i = 0; i < 12; i++) {
+		if (ip->i_block[i] == 0)
+			break;
 
-	// Step to the last entry in a data block 
-	get_block(pip->dev, pip->INODE.i_block[0], buf);
-	cp = buf;
-	dp = (DIR*)cp;
+		// Step to the last entry in a data block 
+		get_block(dev, ip->i_block[i], buf);
+		need_len = 4 * ((8 + dp->name_len + 3) / 4);  // a multiple of 4
 
-	// step to LAST entry in block: int blk = parent->INODE.i_block[i];
-	while ((dp->rec_len + cp) < buf + BLKSIZE)
-	{
-		cp += dp->rec_len;
-		dp = (DIR*)cp;
-	}
-	// To enter a new entry of name with n_len, the needed length is
-	need_len = 4 * ((8 + dp->name_len + 3) / 4);  // a multiple of 4
-
-	// Let remain = LAST entry's rec_len - its IDEAL_LENGTH;
-	remain = dp->rec_len - need_len;
-
-	if (remain >= need_len) {
-		dp->rec_len = need_len;
-
-		cp += dp->rec_len;
-		dp = (DIR*)cp;
-
-		dp->rec_len = remain;
-		dp->inode = myino;
-		dp->name_len = name_len;
-		strncpy(dp->name, myname, dp->name_len);
-
-		put_block(dev, pip->INODE.i_block[0], buf);
-	}
-	else {
-		// For each data block of parent DIR do // assume: only 12 direct blocks
-		for (i = 0; i < 12; i++) {
-			if (pip->INODE.i_block[i] == 0) {
-				pip->INODE.i_block[i] = balloc(dev);
-				pip->refCount = 0;
-				memset(buf, 0, 1024);
-				cp = buf;
-				dp = (DIR*)cp;
-			}
-
-			// Step to the last entry in a data block 
-			get_block(pip->dev, pip->INODE.i_block[i], buf);
-			cp = buf;
+		// step to LAST entry in block: int blk = parent->INODE.i_block[i];
+		while ((dp->rec_len + cp) < buf + BLKSIZE) {
+			cp += dp->rec_len;
 			dp = (DIR*)cp;
-
-			// step to LAST entry in block: int blk = parent->INODE.i_block[i];
-			while ((cp + dp->rec_len) < buf + BLKSIZE)
-			{
-				cp += dp->rec_len;
-				dp = (DIR*)cp;
-			}
-
-			// Each DIR entry has rec_len, name_len. Each entry's ideal length is 
-			idea_len = 4 * ((8 + dp->name_len + 3) / 4);
-
-			// Let remain = LAST entry's rec_len - its IDEAL_LENGTH;
-			remain = dp->rec_len - need_len;
-
-			if (remain >= need_len) {
-				dp->rec_len = idea_len;
-				cp += dp->rec_len;
-				dp = (DIR*)cp;
-			}
 		}
 
-		// Reach here means: NO space in existing data block(s)
-		// Allocate a new data block; IN parent's by BLKSIZE;
-		dp->inode = myino;
-		dp->name_len = name_len;
-		dp->rec_len = remain;
-		strncpy(dp->name, myname, dp->name_len);
+		cp = (char*)dp;
 
-		// write data block to disk
-		put_block(dev, pip->INODE.i_block[i], buf);
+		// Let remain = LAST entry's rec_len - its IDEAL_LENGTH;
+		remain = dp->rec_len - need_len;
+
+		if (remain >= need_len) {
+			dp->rec_len = need_len;
+
+			cp += dp->rec_len;
+			dp = (DIR*)cp;
+
+			dp->rec_len = remain;
+			dp->inode = myino;
+			dp->name_len = name_len;
+			dp->file_type = EXT2_FT_DIR;
+			strncpy(dp->name, myname, dp->name_len);
+
+			put_block(dev, pip->INODE.i_block[0], buf);
+			return 1;
+		}
 	}
 
-	pip->dirty = 1;
-	pip->INODE.i_links_count++;
-	pip->INODE.i_atime = time(0);
+	// Reach here means: NO space in existing data block(s)
+	// Allocate a new data block; IN parent's by BLKSIZE;
+	bnum = balloc(dev);
+	ip->i_block[i] = bnum;
+
+	get_block(dev, bnum, buf);
+
+	dp = (DIR*)buf;
+	cp = buf;
+
+	dp->inode = myino;
+	dp->name_len = name_len;
+	dp->rec_len = remain;
+	dp->file_type = EXT2_FT_DIR;
+	strncpy(dp->name, myname, dp->name_len);
+
+	// write data block to disk
+	put_block(dev, bnum, buf);
+
+	return 1;
 }
 
 /************* creat **************/
@@ -311,11 +287,11 @@ int mycreat(MINODE* pip, char* name) {
 	mip->INODE.i_links_count = 2;	        // Links count=2 because of . and ..
 	mip->INODE.i_atime = mip->INODE.i_ctime = mip->INODE.i_mtime = time(0L);  // set to current time
 	mip->INODE.i_blocks = 2;                	// LINUX: Blocks count in 512-byte chunks
+	mip->INODE.i_block[0] = bno;             // new DIR has one data block
 
-	for (int i = 0; i < 15; i++) //  i_block[1] to i_block[14] = 0;
+	for (int i = 1; i < 15; i++) //  i_block[1] to i_block[14] = 0;
 		mip->INODE.i_block[i] = 0;
 
-	mip->INODE.i_block[0] = bno;             // new DIR has one data block
 
 	mip->dirty = 1;               // mark minode dirty
 	mip->ino = ino;
